ff4f7e99f24d9a2f6f6cf0aa23207e4eb8c9b05f
A-Schloss <60310324+A-Schloss@users.noreply.github.com>
2020-05-29 00:33:31 -0500

    Add files via upload

---
+++ Ejercicios_1.1.2_Funciones.py
@@ -0,0 +1,11 @@
+##_Ejercicio 1.1.2.1.Mensaje_##
+
+def display_message():
+    print("Los temas que se han visto son: funciones y clases")
+display_message()
+
+##_Ejercicio 1.1.2.2 Libro favorito_##
+
+def libro_favorito(titulo):
+    print("Tu libro favorito es " + titulo)
+libro_favorito("Sobre héroes y tumbas")
---
+++ Ejercicios_1.3_Funciones.py
@@ -0,0 +1,22 @@
+##_Ejercicio 1.3.1 Playera_##
+
+print('\nEjercicio 1.3.1 Playera')
+def hacer_Playera(tamanio, texto):
+    print("\nLa playera es talla "+ tamanio +" y el texto a cerigrafear es: "+ texto)
+hacer_Playera("grande","Hola Amikos")
+
+##_Ejercicio 1.3.2 Playeras Grandes_##
+
+print('\nEjercicio 1.3.2 Playeras Grandes')
+def hacerPlayera(talla='grande',texto='Yo <3 Python'):
+    print("\nLa playera es talla "+ talla +' y el texto a cerigrafear es: '+ texto)
+hacerPlayera(talla='grande',texto='Yo <3 Python')
+
+##_Ejercicio 1.3.2 Países y ciudades_##
+
+print('\nEjercicio 1.3.2 Países y ciudades')
+def descripcionCiudad(ciudad,pais='Mexico'):
+    print('\n '+ ciudad + ' esta en '+ pais)
+descripcionCiudad(ciudad='Ciudad de Mexico')
+descripcionCiudad(ciudad='Monterrey')
+descripcionCiudad(ciudad='Berlin')
